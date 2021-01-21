574782a5165963204f2833f709e278654513c95f
u4su06 <doremi90932@gmail.com>
2021-01-21 11:42:04 +0800

    feat: add mongoose

---
+++ models/restaurant.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restaurantSchema = new Schema({
+  id: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  name_en: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  google_map: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+)
+module.exports = mongoose.model('Restaurant', restaurantSchema)
